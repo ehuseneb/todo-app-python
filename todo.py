064ce83faefc00da84d73a57e5b76fec31c01d63
Eneb hussen <enebhussen99@gmail.com>
2026-06-27 09:51:11 +0300

    Upgrade todo app with file storage

--- todo.py
+++ todo.py
@@ -1,4 +1,20 @@
-tasks = []
+# Todo App with File Saving (Upgraded Version)
+
+FILE_NAME = "tasks.txt"
+
+def load_tasks():
+    try:
+        with open(FILE_NAME, "r") as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        return []
+
+def save_tasks(tasks):
+    with open(FILE_NAME, "w") as file:
+        for task in tasks:
+            file.write(task + "\n")
+
+tasks = load_tasks()
 
 while True:
     print("\n--- TODO APP ---")
@@ -12,17 +28,22 @@ while True:
     if choice == "1":
         task = input("Enter a task: ")
         tasks.append(task)
+        save_tasks(tasks)
         print("Task added!")
 
     elif choice == "2":
         print("\nYour Tasks:")
-        for i, task in enumerate(tasks, start=1):
-            print(f"{i}. {task}")
+        if not tasks:
+            print("No tasks found.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
 
     elif choice == "3":
         num = int(input("Enter task number to delete: "))
         if 0 < num <= len(tasks):
             removed = tasks.pop(num - 1)
+            save_tasks(tasks)
             print(f"Removed: {removed}")
         else:
             print("Invalid number")
